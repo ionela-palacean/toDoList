17b252d39b6798bfd8e7e1a331f070a76d48f942
Ionela Palacean <ione.caia85@gmail.com>
2019-10-14 21:44:35 +0300

    studied JDBC

---
+++ src/main/java/org/fasttrackit/todolist/persistance/ToDoItemRepository.java
@@ -0,0 +1,27 @@
+package org.fasttrackit.todolist.persistance;
+
+import org.fasttrackit.todolist.transfer.CreateToDoItemRequest;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ToDoItemRepository {
+
+    public void createToDoItem(CreateToDoItemRequest request) throws SQLException, IOException, ClassNotFoundException {
+       String sql="INSERT INTO `to-do-item` (description, deadline) VALUES (?,?)";
+
+        //try - with - resources
+
+       try (Connection connection=DatabaseConfiguration.getConnnection();
+            PreparedStatement preparedStatement=connection.prepareStatement(sql)) {
+
+           preparedStatement.setString(1,request.getDescription());
+           preparedStatement.setDate(2, Date.valueOf(request.getDeadline()));
+           preparedStatement.executeUpdate();
+       }
+
+    }
+}
